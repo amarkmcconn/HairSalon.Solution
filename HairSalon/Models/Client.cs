577813b641fa525f156fd7c531e9b2133ef5d335
Mark McConnell <amark.mcconnell@gmail.com>
2022-05-20 16:17:01 -0700

    update client and stylist cs to include more properties.

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -8,6 +8,8 @@ namespace HairSalon.Models
     public string Name { get; set; }
     public double PhoneNumber { get; set; }
     public int StylistId { get; set; }
+    public string Pronoun { get; set; }
+    public string Email { get; set; }
 
     public virtual Stylist Stylist { get; set; }
   }
